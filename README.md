512e03dc79605443407efe7de7a7d7587e3709b1
NuclleaR <nuclear.890@gmail.com>
2023-02-19 13:33:06 +0000

    Init nuclear'z colors

--- README.md
+++ README.md
@@ -1,18 +1,6 @@
-# Prism (Light theme)
+# NucleaR'z Light theme
 
-[![Version](https://vsmarketplacebadge.apphb.com/version-short/usernamehw.prism.svg)](https://marketplace.visualstudio.com/items?itemName=usernamehw.prism)
-[![Installs](https://vsmarketplacebadge.apphb.com/installs-short/usernamehw.prism.svg)](https://marketplace.visualstudio.com/items?itemName=usernamehw.prism)
-[![Rating](https://vsmarketplacebadge.apphb.com/rating-short/usernamehw.prism.svg)](https://marketplace.visualstudio.com/items?itemName=usernamehw.prism)
-
-Light theme based on the [Prism.js](https://prismjs.com/) default light theme.
+My custom light theme.
 
 ![react example](./img/react_example.png)
-
-### Recommended settings
-
-<!-- "editor.scrollbar.verticalScrollbarSize": 18, -->
-```js
-"editor.lineHeight": 1.5,
-"editor.minimap.renderCharacters": false,
-"workbench.tips.enabled": false,
-```
+![react example 2](./img/react_example_2.png)
--- minimize.js
+++ minimize.js
@@ -1,15 +1,20 @@
-const JSON5 = require('json5');
-const fs = require('fs');
+const JSON5 = require("json5");
+const fs = require("fs");
 
-fs.readFile('themes/Prism-color-theme.json', 'utf8', (err, content) => {
+fs.readFile("themes/Nuclear-color-theme-light.json", "utf8", (err, content) => {
 	const parsed = JSON5.parse(content);
-	fs.writeFile('themes/Prism-color-theme-minimized.json', JSON.stringify(parsed), 'utf8', () => {
-		console.log('✔  DONE');
-	});
+	fs.writeFile(
+		"themes/Nuclear-color-theme-light-minimized.json",
+		JSON.stringify(parsed),
+		"utf8",
+		() => {
+			console.log("✔  DONE");
+		}
+	);
 	const noBoldTokenColors = [];
 	for (const item of parsed.tokenColors) {
 		if (!item.settings) continue;
-		if (item.settings.fontStyle === 'bold') {
+		if (item.settings.fontStyle === "bold") {
 			const scope = item.scope;
 			const settings = {};
 			if (item.settings) {
@@ -24,16 +29,21 @@ fs.readFile('themes/Prism-color-theme.json', 'utf8', (err, content) => {
 		}
 	}
 	noBoldTokenColors.push({
-		"scope": "markup.bold",
-		"settings": {
-			"fontStyle": "bold"
-		}
+		scope: "markup.bold",
+		settings: {
+			fontStyle: "bold",
+		},
 	});
 	const noBold = {
 		...parsed,
 		tokenColors: noBoldTokenColors,
-	}
-	fs.writeFile('themes/Prism-color-theme-no-bold-minimized.json', JSON.stringify(noBold), 'utf8', () => {
-		console.log('✔  NO BOLD DONE');
-	});
+	};
+	fs.writeFile(
+		"themes/Nuclear-color-theme-light-no-bold-minimized.json",
+		JSON.stringify(noBold),
+		"utf8",
+		() => {
+			console.log("✔  NO BOLD DONE");
+		}
+	);
 });
